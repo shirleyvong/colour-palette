59b7bd67e50fa858fdada558517d0f2456bd7db5
Shirley Vong <shirleyv97@hotmail.com>
2020-08-04 23:02:24 -0700

    Add logic to enter and submit image URL

--- client/src/components/ImageLink.jsx
+++ client/src/components/ImageLink.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const ImageLink = () => {
+const ImageLink = ({ uploadURL }) => {
+  const [input, setInput] = useState('');
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    setInput(event.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    if (input === '') {
+      return;
+    }
+
+    // TODO: verify URL is an image, either client or server side
+    uploadURL(input);
+  };
+
   return (
     <>
       <form>
-        <input type="url" placeholder="Enter an image URL" />
+        <input onChange={handleInput} type="url" placeholder="Enter an image URL" />
+        <button onClick={handleButtonClick} type="button">Submit</button>
       </form>
     </>
   );
+};
+
+ImageLink.propTypes = {
+  uploadURL: PropTypes.func.isRequired,
 };
 
 export default ImageLink;
