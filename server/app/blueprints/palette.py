b92ef238495c844d1f43c1464f11e63136f4fd71
Shirley Vong <shirleyv97@hotmail.com>
2020-08-06 01:05:26 -0700

    Update request to call colour quantization algorithm and return results

--- server/app/blueprints/palette.py
+++ server/app/blueprints/palette.py
@@ -1,7 +1,30 @@
 from flask import Blueprint, request, current_app
-
+from app.colour_quantization import get_colour_palette
 palette = Blueprint('palette', __name__)
 
-@palette.route('/')
+@palette.route('', methods=['POST'])
 def create_palette():
-  return 'hello'
+  if 'file' not in request.files:
+    return 'request does not contain file part'
+  
+  file = request.files['file']
+  if file.filename == '':
+    return 'no file was selected'
+
+  # TODO: validate image 
+  current_app.logger.info(file.filename)
+  colour_palette = get_colour_palette(file)
+  current_app.logger.info(colour_palette)
+
+  # convert to dictionary
+  result = []
+  for colour in colour_palette:
+    result.append({
+      'red': colour[0],
+      'green': colour[1],
+      'blue': colour[2],
+    })
+
+  return {
+    'colours': result
+  }
