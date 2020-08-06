4cf0a1481dd5df69b31e51fa09de9c6b0448d717
Shirley Vong <shirleyv97@hotmail.com>
2020-08-05 23:26:43 -0700

    Send request to upload image file to server

--- client/src/ColourPalettePage.jsx
+++ client/src/ColourPalettePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ImageSelect from './components/ImageSelect';
 import Results from './components/Results';
+import axios from 'axios';
 
 const ColourPalettePage = () => {
   const [colours, setColours] = useState([]);
@@ -13,10 +14,25 @@ const ColourPalettePage = () => {
   };
 
   const uploadFile = (file) => {
-    // TODO: make request to server
     console.log('Uploading file');
     setImageFile(file);
     setImageURLFromFile(file);
+
+    const formData = new FormData();
+    formData.append('file', file);
+    
+    axios.post('/palette',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => { console.log(err) });
+
     setColours(['#f5bc42', '#e8cd92', '#92d8e8', '#e892bd', '#f2a0c9']);
   };
 
