13cd2fd120d7c9fa2f54a4fb72fcafd603b71998
Shirley Vong <shirleyv97@hotmail.com>
2020-08-11 15:31:25 -0700

    Implement quantization algorithm in Rust

--- quantize/src/lib.rs
+++ quantize/src/lib.rs
@@ -1,5 +1,7 @@
-use cpython::{PyResult, Python, py_module_initializer, py_fn, PyDict, PyList, PyInt};
+use cpython::{PyResult, Python, py_module_initializer, py_fn, PyDict, PyList};
 use std::collections::HashMap;
+use std::cmp::Ordering;
+use cpython::PythonObject;
 
 // Expands to an extern "C" function that allows Python to load the rust code as 
 // a Python extension module
@@ -11,14 +13,13 @@ py_module_initializer!(libquantize, |py, m| {
     // Add functions as members of module 
     // py_fn! returns a value of type PyObject. When this python object is called, it invokes
     // the rust function.
-    m.add(py, "quantize", py_fn!(py, quantize(py_pixels: PyList)))?;
+    m.add(py, "quantize", py_fn!(py, quantize(py_pixels: PyList, num_partitions: usize)))?;
     Ok(())
 });
 
-fn quantize(py: Python, py_pixels: PyList) -> PyResult<usize>{
+fn quantize(py: Python, py_pixels: PyList, num_partitions: usize) -> PyResult<PyList>{
     // convert python list of dicts with rgb values to rust vectors of hash maps
     let mut rs_pixels = Vec::new();
-
     for obj in py_pixels.iter(py) {
         let py_pixel = obj.extract::<PyDict>(py).unwrap();
         let mut rs_pixel = HashMap::new();
@@ -38,7 +39,108 @@ fn quantize(py: Python, py_pixels: PyList) -> PyResult<usize>{
         rs_pixels.push(rs_pixel);
     }
 
-    println!("{:#?}", rs_pixels);
+    // println!("{:#?}", rs_pixels);
+    let partitions = partition(rs_pixels, num_partitions);
 
-    Ok(0)
+    let rgb_colours: Vec<HashMap<&str, u32>> = partitions.into_iter().map(|p: Vec<HashMap<&str, u32>>| -> HashMap<&str, u32> {
+        let total_red = p.iter().fold(0, |acc, x| acc + x.get("red").unwrap());
+        let total_green = p.iter().fold(0, |acc, x| acc + x.get("green").unwrap());
+        let total_blue = p.iter().fold(0, |acc, x| acc + x.get("blue").unwrap());
+
+        let length = p.len() as u32;
+        let mut avg_colour = HashMap::new();
+        avg_colour.insert("red", total_red / length);
+        avg_colour.insert("green", total_green / length);
+        avg_colour.insert("blue", total_blue / length);
+
+        avg_colour
+    }).collect();
+
+    println!("{:#?}", rgb_colours);
+
+    // Convert vector of hashmaps back to python list of dicts
+    let mut py_dict_vec = Vec::new();
+    for colour in rgb_colours.iter() {
+        let py_dict = PyDict::new(py);
+        for (key, val) in colour.iter() {
+            py_dict.set_item(py, key, val).unwrap();
+        }
+        py_dict_vec.push(py_dict.into_object());
+    }
+
+    Ok(PyList::new(py, &py_dict_vec[..]))
+}
+
+fn partition(pixels: Vec<HashMap<&str, u32>>, num_partitions: usize) -> Vec<Vec<HashMap<&str, u32>>> {
+    let mut partitions = Vec::new();
+    partitions.push(pixels);
+
+    while (&partitions).len() < num_partitions {
+        let (idx, colour) = largest_range_partition(&partitions);
+        let mut split_partition = partitions.remove(idx);
+        split_partition.sort_by(|l, r| -> Ordering {
+            let lval = l.get(&colour[..]).unwrap();
+            let rval = r.get(&colour[..]).unwrap();
+            (*lval).cmp(rval)
+        });
+
+        let median = split_partition.len() / 2;
+        partitions.push(split_partition.split_off(median));
+        partitions.push(split_partition);
+        println!("Split on {}", colour);
+    }
+
+    partitions
+}
+
+// Get the partition with the largest range and the corresponding colour
+fn largest_range_partition(partitions: &Vec<Vec<HashMap<&str, u32>>>) -> (usize, String) {
+    // TODO: add better initialized values
+    let mut largest_range = u32::MIN;
+    let mut largest_range_idx = 0;
+    let mut largest_range_colour = String::from("");
+
+    for (idx, partition) in partitions.iter().enumerate() {
+        let (range, colour) = largest_colour_range(partition);
+        if range > largest_range {
+            largest_range = range;
+            largest_range_colour = colour;
+            largest_range_idx = idx;
+        }
+    }
+
+    (largest_range_idx, largest_range_colour)
+}
+
+// Get the largest range and corresponding colour from a partition of pixels
+fn largest_colour_range(partition: &Vec<HashMap<&str, u32>>) -> (u32, String) {
+    let red_range = colour_range(partition, "red");
+    let green_range = colour_range(partition, "green");
+    let blue_range = colour_range(partition, "blue");
+
+    if red_range >= green_range && red_range >= blue_range {
+        (red_range, String::from("red"))
+    } else if green_range >= red_range && green_range >= blue_range {
+        (green_range, String::from("green"))
+    } else {
+        (blue_range, String::from("blue"))
+    }
+}
+
+// Get the range of a colour from a partition of pixels.
+fn colour_range(partition: &Vec<HashMap<&str, u32>>, colour: &str) -> u32 {
+    let mut min = u32::MAX;
+    let mut max = u32::MIN;
+
+    for pixel in partition.iter() {
+        let value = *pixel.get(colour).unwrap();
+        if value <= min {
+            min = value;
+        } else if value >= max {
+            max = value
+        }
+    } 
+
+    let range = max - min;
+    range
 }
